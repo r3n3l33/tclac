19899bf7e4d18064c0f8c83abce660190d292f19
Stefan Altheimer <me@stefan-altheimer.de>
2025-06-16 09:48:52 +0200

    A

--- components/tclac/tclac.cpp
+++ components/tclac/tclac.cpp
@@ -98,10 +98,10 @@ void tclacClimate::update() {
 	this->esphome::uart::UARTDevice::write_array(poll, sizeof(poll));
 	//const char* raw = tclacClimate::getHex(poll, sizeof(poll)).c_str();
 	delay(500);
-	int i = 0;
+	int c = 0;
 	while (esphome::uart::UARTDevice::available() > 0) {
-		esphome::uart::UARTDevice::read_byte(&dataRX[i]);
-		i++;
+		esphome::uart::UARTDevice::read_byte(&dataRX[c]);
+		c++;
 	}
 
 	tclacClimate::readData();
@@ -109,10 +109,10 @@ void tclacClimate::update() {
 
 	this->esphome::uart::UARTDevice::write_array(polltwo, sizeof(polltwo));
 	delay(500);
-	i = 0;
+	c = 0;
 	while (esphome::uart::UARTDevice::available() > 0) {
-		esphome::uart::UARTDevice::read_byte(&dataRX[i]);
-		i++;
+		esphome::uart::UARTDevice::read_byte(&dataRX[c]);
+		c++;
 	}
 
 	tclacClimate::readData();
