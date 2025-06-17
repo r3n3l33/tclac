331ac78d4f8c91ec2c9c847967c0e850f28ba525
Stefan Altheimer <me@stefan-altheimer.de>
2025-06-17 13:50:21 +0200

    A

--- components/tclac/tclac.cpp
+++ components/tclac/tclac.cpp
@@ -114,7 +114,7 @@ void tclacClimate::readData() {
 	//current_temperature = float((( (dataRX[17] << 8) | dataRX[18] ) / 374 - 32)/1.8);
 	//target_temperature = (dataRX[FAN_SPEED_POS] & SET_TEMP_MASK) + 16;
 	current_temperature = float((dataRX[37] << 8 | (dataRX[36]))*0.001);
-	//current_temperature = float((( (dataRX[36] << 8) | dataRX[37] ) / 374 - 32)/1.8);
+	current_temperature2 = float((( (dataRX[37] << 8) | dataRX[36] ) / 374 - 32)/1.8);
 	this->current_temperature = current_temperature;
 	//target_temperature = 20;
 
@@ -124,6 +124,7 @@ void tclacClimate::readData() {
 	//target_temperature = (dataRX[FAN_SPEED_POS] & SET_TEMP_MASK) + 16;
 
 	ESP_LOGD("TCL", "TEMP: %f ", current_temperature);
+	ESP_LOGD("TCL", "TEMP: %f ", current_temperature2);
 
 
 	if (dataRX[MODE_POS] & ( 1 << 4)) {
